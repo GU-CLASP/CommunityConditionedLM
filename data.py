3033afd1974c8b33e1680cf204b372f0542a74c7
Bill Noble <winobes@gmail.com>
2020-10-03 20:39:33 +0200

    handle dupes outside data loader

--- data.py
+++ data.py
@@ -28,24 +28,22 @@ def build_fields(fields, data, vocab_size):
     fields['text'].build_vocab(data, max_size=vocab_size, specials=['<eos>', '<pad>'])
     return fields
 
-def save_fields(fields):
-    for field in fields:
-        torch.save(fields[field], f"model/{field}.field")
-
-def load_fields():
-    fields = ((field, torch.load(f"model/{field}.field")) for field in ('community', 'text'))
-    return OrderedDict(fields)
-
-def load_data_and_fields(data_dir, max_seq_len, vocab_size, rebuild_vocab, file_limit):
-    if not rebuild_vocab and os.path.exists('model/text.field'):
+def load_data_and_fields(data_dir, field_dir, max_seq_len, vocab_size, rebuild_vocab, file_limit):
+    text_field_file = os.path.join(field_dir, 'text.field')
+    comm_field_file = os.path.join(field_dir, 'community.field')
+    if not rebuild_vocab and os.path.exists(text_field_file):
         print("Using existing fields.")
-        fields = load_fields()
+        fields = OrderedDict(
+            ('text', torch.load(text_field_file)),
+            ('community', toch.load(comm_field_file))
+        )
         dataset = tt.data.Dataset(list(gen_examples(data_dir, fields, max_seq_len, file_limit)), fields=fields)
     else:
         print("Creating new fields.")
         fields = create_fields()
         dataset = tt.data.Dataset(list(gen_examples(data_dir, fields, max_seq_len, file_limit)), fields=fields)
         fields = build_fields(fields, dataset, vocab_size)
-        save_fields(fields)
+        torch.save(fields['text'], text_field_file)
+        torch.save(fields['community'], comm_field_file)
     return dataset, fields
 
