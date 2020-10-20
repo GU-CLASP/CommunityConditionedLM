1ea0a57010e8d0145cf2c74263207a125b27d575
Bill Noble <winobes@gmail.com>
2020-10-20 15:21:43 +0200

    add test_perplexity command

--- eval_lm.py
+++ eval_lm.py
@@ -11,10 +11,62 @@ import json
 import os
 import click
 
-def test_perplexity():
+@click.group()
+def cli():
     pass
 
-@click.command()
+def test_perplexity():
+
+    def perplexity_per_word(y, y_hat, lengths, text_pad_idx):
+        vocab_size = y_hat.shape[2]
+        nll = F.nll_loss(y_hat.view(-1,vocab_size), y.view(-1), ignore_index=text_pad_idx, reduction='none').view(y.shape)
+        nll_word = nll.sum(axis=0) / lengths.to(torch.float) # negative log-likelihood per word
+        return nll_word.exp() # perplexity per word
+
+    def test_ppl(lm, test_batches, text_pad_idx):
+        ppls = []
+        for i, batch in enumerate(test_batches):
+            print(f'{i+1}/{len(test_batches)}', end='\r')
+            x_comm = batch.community
+            text, lengths = batch.text
+            x_text = text[:-1]
+            y = text[1:]
+            y_hat = lm(x_text, x_comm)
+            ppl_word = perplexity_per_word(y, y_hat, lengths, text_pad_idx)
+            ppls += [ppl.item() for ppl in ppl_word]
+        return ppls
+
+    df = pd.DataFrame([(example.community, ' '.join(example.text)) 
+        for example in test_data], columns=['community', 'comment'])
+    models = {}, []
+    for name in architectures:
+        ppls = test_ppl(lm, test_iterator, text_pad_idx)
+        model_names.append(name)
+        models[name] = lm
+        print(f"Done {name}")
+        df[name] = ppls
+    df.to_pickle(results_file)
+
+@cli.command()
+@click.argument('model_family_dir', type=click.Path(exists=False))
+@click.argument('data_dir', type=click.Path(exists=True))
+@click.option('--max-seq-len', default=64)
+@click.option('--file-limit', type=int, default=50000,
+        help="Number of examples per file (community).")
+def write_test_set_df(model_family_dir, data_dir, max_seq_len, file_limit):
+
+    random.seed(42)
+    random_state = random.getstate()
+    dataset, fields = load_data_and_fields(data_dir, model_family_dir, max_seq_len, file_limit)
+    _, _, test_data = dataset.split(split_ratio=[0.8,0.1,0.1],
+            stratified=True, strata_field='community', random_state=random_state)
+
+    df = pd.DataFrame([(ex.community, ' '.join(ex.text)) for ex in test_data], 
+            columns = ['community', 'text'])
+    df.to_pickle(os.path.join(model_family_dir, 'test_data_df.pickle'))
+
+
+@cli.command()
 @click.argument('model_family_dir', type=click.Path(exists=False))
 @click.argument('model_name', type=str)
 @click.argument('data_dir', type=click.Path(exists=True))
@@ -24,7 +76,7 @@ def test_perplexity():
         help="Number of examples per file (community).")
 @click.option('--gpu-id', type=int, default=None,
         help="ID of the GPU, if traning with CUDA")
-def cli(model_family_dir, model_name, data_dir, batch_size, max_seq_len, 
+def infer_comm(model_family_dir, model_name, data_dir, batch_size, max_seq_len, 
         file_limit, gpu_id):
 
     model_dir = os.path.join(model_family_dir, model_name)
