d324e875fea3e8d8e5f0eeb15d6ed0c3cc14c094
Bill Noble <winobes@gmail.com>
2020-11-03 08:12:59 +0100

    make comm/author dicts

--- comm_author_embed.py
+++ comm_author_embed.py
@@ -18,16 +18,41 @@ def iter_counts():
         with open(file, 'r') as f:
             reader = csv.DictReader(f)
             for item in reader:
-                yield item['author'], item['subreddit'], item['comment_count']
+                yield item['author'], item['subreddit'], int(item['comment_count'])
 
+author_stoi, author_itos = {}, []
+comm_stoi, comm_itos = {}, []
+
+print("Compiling counts.")
 data, row, col = [], [], []
 for author, comm, count in iter_counts():
+    if author in author_stoi:
+        author_idx = author_stoi[author]
+    else:
+        author_itos.append(author)
+        author_idx = len(author_itos)
+        author_stoi[author] = author_idx
+    if comm in comm_stoi:
+        comm_idx = comm_stoi[comm]
+    else:
+        comm_itos.append(comm)
+        comm_idx = len(comm_itos)
+        comm_stoi[comm] = comm_idx
     data.append(count)
-    row.append(comm)
-    col.append(author)
+    row.append(comm_idx)
+    col.append(author_idx)
+
+def save_list(l, file):
+    with open(file, 'w') as f:
+        f.write('\n'.join(l))
+
+save_list(author_itos, 'model/reddit2015/authors_full.txt')
+save_list(comm_itos, 'model/reddit2015/comms_full.txt')
+
 
 print("Creating counts matrix.")
-author_comm_mat = coo_matrix((data, (row, col))).tocsr()
+author_comm_mat = coo_matrix((data, (row, col)))
+author_comm_mat = author_comm_mat.tocsr()
 np.save('model/reddit2015/comm_author_counts_full', author_comm_mat)
 
 print("Computing SVD.")
