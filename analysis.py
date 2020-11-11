ebb53b59c2df28f0e6b44153e5df70d2ba2bc491
Bill Noble <winobes@gmail.com>
2020-11-11 16:02:37 -0500

    add pairwise similarity plot/table

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 import numpy as np
 import torch
+from scipy.stats import pearsonr
+
 
 model_dir = 'model/reddit2015'
 floats_dir = 'paper/floats'
@@ -32,8 +34,15 @@ def model_params_from_name(model_name):
         lc = layers_str[-1]
     return encoder_arch, lc 
 
-models = [model_params_to_name(arch, lc) for arch in architectures for lc in lcs]
+models = [model_params_to_name(arch, lc) for arch in architectures for lc in lcs] 
 conditioned_models = [model_params_to_name(arch, lc) for arch in architectures for lc in lcs if lc is not None]
+
+comms = torch.load(open(os.path.join(model_dir, 'community.field'), 'rb')).vocab.itos[1:]
+comms = pd.Series(comms, name='community')
+
+
+cclm_comm_embed = {model: np.load(os.path.join(model_dir, f'{model}/comm_embed.npy'))[1:]
+        for model in conditioned_models}
 
 ##### BEST EPOCH
 
@@ -43,6 +52,7 @@ def get_best_epoch(model_name):
 best_epoch = pd.DataFrame([{str(lc): get_best_epoch(model_params_to_name(arch, lc))
     for lc in lcs} for arch in architectures], index=architectures)
 best_epoch.to_latex(os.path.join(floats_dir, 'best_epoch.tex'))
+
 
 ##### Perplexity by model
 
@@ -59,13 +69,10 @@ ppl_mean_comm = ppl_mean_comm.reindex(rows)
 ppl_mean_comm.to_latex(os.path.join(floats_dir, 'model_ppls.tex'), 
         float_format="%.2f", multicolumn_format='c')
 
+
 ##### Community embedding PCA
 
 from sklearn.decomposition import PCA
-
-comms = torch.load(open(os.path.join(model_dir, 'community.field'), 'rb')).vocab.itos[1:]
-comms = pd.Series(comms, name='community')
-comms_alpha = sorted(comms)
 
 # Manually assign communities to different types/subjects
 # NOTE this shouldn't really be a partition. Some communities clearly belong to multiple types,
@@ -91,7 +98,7 @@ def pca(w):
     return pca.fit_transform(w)
 
 def write_pca_table(model_name):
-    comm_embed = np.load(os.path.join(model_dir, f'{model_name}/comm_embed.npy'))[1:]
+    comm_embed = cclm_comm_embed[model_name]
     comm = pd.DataFrame(pca(comm_embed), index=comms, columns=['pca1', 'pca2'])
     comm['category'] = comm_cats
     comm = comm.reset_index()[['pca1', 'pca2', 'community', 'category']]
@@ -104,6 +111,8 @@ for model in conditioned_models:
 
 
 #### Community inference confusion matrix
+
+from comm_author_embed import load_snap_comm_embed
 
 comms_alpha = sorted(list(comms))
 
@@ -120,8 +129,58 @@ def model_comm_confusion_matrix(model_name):
     C['confered_to'] = C['confered_to'].apply(comms_alpha.index)
     return C
 
-
-
 for model_name in conditioned_models:
     C = model_comm_confusion_matrix(model_name)
     C.to_csv(os.path.join(floats_dir, f'{model_name}_comm_infer_confusion.csv'), sep='\t', float_format="% 5.4f", index=False)
+
+#### Pairwise community similarity scatter
+
+from itertools import combinations
+
+def cos_sim(v1, v2):
+    return (v1 * v2).sum(axis=0) / (np.linalg.norm(v1, axis=0) * np.linalg.norm(v2, axis=0))
+
+def pairwise_sims(embedding, pairs):
+    return pd.Series([cos_sim(*map(lambda x: embedding[list(comms).index(x)], pair)) for pair in pairs], index=pairs)
+    
+df = pd.DataFrame([], index=pairs)
+for model in conditioned_models:
+    df[model] = pairwise_sims(cclm_comm_embed[model], pairs)
+
+snap_comm_embed = load_snap_comm_embed(comms)
+
+df['snap'] = pairwise_sims(snap_comm_embed, pairs)
+
+def pair_cats(pair):
+    cat1, cat2 = map(dict(comm_cats).get, pair)
+    if cat1 == 'other' or cat2 == 'other':
+        return 'other'
+    elif cat1 == cat2:
+        return cat1
+    else:
+        return 'different'
+
+df['meta'] = pd.Series(map(pair_cats, pairs), index=pairs)
+
+
+pairs_str = pd.Series(df.index).apply(lambda x: f'{x[0]}/{x[1]}')
+pairs_str.index=pairs
+df['pair'] = pairs_str
+
+df.to_csv(os.path.join(floats_dir, f'comm_sim.csv'), sep='\t', 
+            float_format="% 5.4f", index=False)
+
+df_r = pd.DataFrame([pearsonr(df[model], df['snap']) 
+    for model in conditioned_models], index=conditioned_models,
+    columns = ['r', 'p'])
+
+def pivot_model_table(df):
+    df = df.reset_index()
+    df['arch'] = df['index'].apply(lambda x: x.split('-')[0])
+    df['arch'] = df['arch'].apply({'lstm': 'LSTM', 'transformer': 'Transformer'}.get)
+    df['c'] = df['index'].apply(lambda x: x.split('-')[-1])
+    df = df.drop('index', axis=1)
+    return df.pivot(index='arch', columns='c')
+
+pivot_model_table(df_r)['r'].to_latex(os.path.join(floats_dir, 'comm_sim.tex')
+        float_format="%.2f", multicolumn_format='c')
