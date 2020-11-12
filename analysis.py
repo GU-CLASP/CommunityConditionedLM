d4b2c2b9acb592b2a3edc3ef1d1cb9b07194a988
Bill Noble <winobes@gmail.com>
2020-11-12 15:37:39 -0500

    resolve LMCC entropy question

--- analysis.py
+++ analysis.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 from scipy.stats import pearsonr
 
-
 model_dir = 'model/reddit2015'
 floats_dir = 'paper/floats'
 
@@ -39,7 +38,6 @@ conditioned_models = [model_params_to_name(arch, lc) for arch in architectures f
 
 comms = torch.load(open(os.path.join(model_dir, 'community.field'), 'rb')).vocab.itos[1:]
 comms = pd.Series(comms, name='community')
-
 
 cclm_comm_embed = {model: np.load(os.path.join(model_dir, f'{model}/comm_embed.npy'))[1:]
         for model in conditioned_models}
@@ -194,111 +192,29 @@ pivot_model_table(df_r)['r'].to_latex(os.path.join(floats_dir, 'comm_sim.tex'),
 
 from scipy.special import entr
 
+def entropy(x):
+    return entr(x).sum()
+
 def ppl(x):
-    return np.exp(entr(x).sum())
+    return np.exp(etropy(x))
 
-
-# This is what we were looking at in the notebook.
-# It gives the "perplexity" of the confusion matrix row.
-# The problem is, since this is an average over the (comment-level) CC distributions,
-# it is not itself a probability distribution.
-lmcc_confusion_ppl = []
+lmcc_mean_entr = []
 for model in conditioned_models:
-    # Why is there a single comm_probs.pickle file? Isn't it model-dependent? What is in this file?
-    #  > There is one for each model in the model directory.
-    #  > This file has a row for each comment with the columns "actual_comm" and the remaining columns
-    #  > are a probability distribution over communities (the normalisation of P(c_i | m), as below).
-    
-    # We should: 
-    # 1. Compute P(c_i | m)
-    #   Given: P(m | c_i); ie. a probability assigned assigned to m, when setting the CCLM to c_i
-    #   (this is just the exponential of the negative loss.)
-    #   The actual community of m is given by the "actual_comm" column in the 'dataframe'.
-    # Then we can to compute P(c_i | m) by normalising the list
-    #  of P(m | c_i) (varying i but keeping j fixed) so that ∑_i P(c_i|m) = 1
-    # 2. We have too many numbers, so we take the accuracy of the LMCC prediction per *actual* community
-    #    P(c_i | c_j)
-    #      = P(c_i|m) for a random message m in community c_j.
-    #      = ∑_m P(c_i|m) P(m|c_j)
-    #                   but here we take every message to be equally "probable" in c_j, so P(m|c_j)=1/|c_j|
-    #                   if we take |c_j| to be the number of messages in community c_j 
-    #      = ∑_m P(c_i|m) / |c_j|
-    #    Is this a probability distribution (when varying c_i?):
-    #    ∑_i P(c_i | c_j)
-    #                                                                by def.
-    #      = ∑_i (∑_m P(c_i|m) / |c_j|)
-    #                                                                by linearity of sum.
-    #      = ∑_m ∑_i P(c_i|m) / |c_j|
-    #                                                                total probability
-    #      = ∑_m 1 / |c_j|
-    #                                                                size of the domain of m
-    #      = |c_j| / |c_j|
-    #      = 1
-    # 
-    # > OK, I see that you're right. The mean of discrete probability distributions 
-    # > is itself a probability distribution. I'm not sure it's right to call this
-    # > distribution accuracy, however, since I would a prediction (via argmax, for
-    # > example) to be involved.
-    # > 
-    # > It's possible I've convinced myself there's a problem where there isn't one,
-    # > but I see two problems with using the entropy of the average distribution:
-    # >
-    # > 1. This metric can be the same for communities for which the classifier has 
-    # > different accuracy.
-    # >
-    # >   comm_prob =
-    # >   m | p(c1 | m) | p(c2 | m ) | cj (actual_comm) 
-    # >   ----------------------------------------
-    # >   0 |       0.7 |       0.3  | c1
-    # >   1 |       0.8 |       0.2  | c1
-    # >   2 |       0.7 |       0.3  | c2
-    # >   3 |       0.8 |       0.2  | c2
-    # >
-    # >   comm_prob.groupby('actual_comm').mean() =
-    # >            cj | p(c1 | cj) | p(c2 | cj ) 
-    # >   ----------------------------------------
-    # >            c1 |      0.75 |       0.25  
-    # >            c2 |      0.75 |       0.25
-    # >
-    # >   comm_prob.groupby('actual_comm').mean().apply(ppl) =
-    # >    cj  | ppl 
-    # >   ----------
-    # >     c1 | 1.754    
-    # >     c2 | 1.754   
-    # >
-    # > Even though the classifier is more accurace for c1 than c2, the perplexity is
-    # > the same for both.
-    # >
-    # > 2. Why should we care about the perplexity of the mean distriubtion rather
-    # > than the mean of of the perplextity? When we consider the perplexity of the
-    # > CCLM, it is the later (via, cross entropy loss over the vocabulary).
+    model_entr = pd.read_pickle(os.path.join(os.path.join(model_dir, model), 'comm_probs.pickle')).groupby('actual_comm').mean().apply(entropy, axis=1)[comms]
+    model_entr.name = model
+    lmcc_mean_entr.append(model_entr)
 
-    model_ppl = pd.read_pickle(os.path.join(os.path.join(model_dir, model), 'comm_probs.pickle')).groupby('actual_comm').mean().apply(ppl, axis=1)[comms]
-    model_ppl.name = model
-    lmcc_confusion_ppl.append(model_ppl)
+lmcc_mean_entr = pd.concat(lmcc_mean_entr, axis=1)
+lmcc_mean_entr.index.name = 'community'
 
-lmcc_confusion_ppl = pd.concat(lmcc_confusion_ppl, axis=1)
-lmcc_confusion_ppl.index.name = 'community'
-
-# I think this is what we should be looking at for lmcc_ppl instead (and what is defined in the paper, if I understand correctly).
-# It computes the CC perplexity for each comment and averages it over the (actual) community.
-lmcc_ppl = []
-for model in conditioned_models:
-    model_ppl = pd.read_pickle(os.path.join(os.path.join(model_dir, model), 'comm_probs.pickle'))[comms].apply(ppl, axis=1)
-    model_ppl.name = model
-    lmcc_ppl.append(model_ppl) 
-lmcc_ppl= pd.concat(lmcc_ppl, axis=1)
-lmcc_ppl['community'] = comms 
-
-lmcc_ppl_mean = lmcc_ppl.groupby('community').mean()
 cclm_ppl_mean = cclm_ppl.groupby('community').mean()
 
-df = pd.merge(lmcc_ppl_mean, cclm_ppl_mean, right_index=True, left_index=True, suffixes=['_lmcc', '_cclm'])
+df = pd.merge(lmcc_mean_entr, cclm_ppl_mean, right_index=True, left_index=True, suffixes=['_lmcc', '_cclm'])
 df.to_csv(os.path.join(floats_dir, f'cclm_lmcc_ppl.csv'), sep='\t', 
             float_format="% 5.4f", index=True)
 
 for model in conditioned_models:
-    r, p = pearsonr(lmcc_confusion_ppl[model],cclm_ppl_mean[model])
+    r, p = pearsonr(lmcc_mean_entr[model],cclm_ppl_mean[model])
     print(f"{model:<15} r = {r:0.2f} p = {p:0.4f}")
 # lstm-3-0        r = -0.04 p = 0.8032
 # lstm-3-1        r = -0.02 p = 0.8900
@@ -309,19 +225,7 @@ for model in conditioned_models:
 # transformer-3-2 r = -0.03 p = 0.8392
 # transformer-3-3 r = -0.04 p = 0.7830
 
-for model in conditioned_models:
-    r, p = pearsonr(lmcc_ppl_mean[model],cclm_ppl_mean[model])
-    print(f"{model:<15} r = {r:0.2f} p = {p:0.4f}")
-# lstm-3-0        r = 0.35 p = 0.0185
-# lstm-3-1        r = 0.30 p = 0.0444
-# lstm-3-2        r = 0.29 p = 0.0504
-# lstm-3-3        r = 0.25 p = 0.0878
-# transformer-3-0 r = 0.28 p = 0.0591
-# transformer-3-1 r = 0.32 p = 0.0298
-# transformer-3-2 r = 0.28 p = 0.0620
-# transformer-3-3 r = 0.30 p = 0.0456
-
-r, p = pearsonr(lmcc_ppl_mean['lstm-3-1'], lmcc_ppl_mean['transformer-3-3'])
+r, p = pearsonr(lmcc_mean_entr['lstm-3-1'], lmcc_mean_entr['transformer-3-3'])
 print(f"r = {r:0.4f} p = {p:0.6f}")
-# r = 0.9922 p = 0.000000
+# r = 0.9935 p = 0.000000 
 
