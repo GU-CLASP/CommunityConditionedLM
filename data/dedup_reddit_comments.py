c19bcad2f8af7458336977997b364078cfc0bb15
Bill Noble <winobes@gmail.com>
2021-02-18 09:00:17 -0500

    dedup comments

---
+++ data/dedup_reddit_comments.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+def iter_comments(corpus_dir):
+    for comment_file in corpus_dir.glob('*.csv'):
+        print(comment_file)
+        with open(comment_file, 'r') as f:
+            reader = csv.DictReader(f)
+                for comment in reader:
+                    yield comment['id'], comment['body']
+
+
+def find_dupe_comments(comments):
+    min_length = 50
+    seen_hashes = set()
+    dupe_ids = []
+        for comment_id, body in comments: 
+            comment_len = len(body) 
+            if comment_len < min_length:
+                continue
+            # only consider the last 50 characters for duplicates (catches most bot-filled forms)
+            comment_hash = hash(body[-min_length:])
+            if comment_hash in seen_hashes:
+                dupe_ids.append(comment_id)
+            else:
+                seen_hashes.add(comment_hash) 
+    return dupe_ids
+
+corpus_dir = Path('synth_data')
+comment_iter = iter_comments(corpus_dir)
+dupe_ids = find_dupe_comments(coment_iter)
+with open(corpus_dir/'dupes.txt', 'w') as f:
+    f.write('\n'.join(dupe_ids))
+
