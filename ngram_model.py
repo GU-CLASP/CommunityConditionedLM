e56606435d5905e92fa0eedd123706f7332ab28f
Bill Noble <winobes@gmail.com>
2021-02-19 06:26:59 -0500

    start working on ngram

--- ngram_model.py
+++ ngram_model.py
@@ -1,6 +1,65 @@
-import os
-from torchtext.data.utils import ngrams_iterator
-from collections import Counter, defaultdict
+from pathlib import Path
+import util
+import data
+import random
+import itertools
+import numpy as np
 
-def bigrams_iter(tokens):
-    return zip(*[tokens[i:] for i in range(2)])
+def iter_ngrams(text, n=3):
+    ts = itertools.tee(text, n)
+    for i, t in enumerate(ts[1:]):
+        for _ in range(i+1):
+            next(t)
+    return zip(*ts)
+
+# @click.argument('model_dir', type=click.Path(exists=True))
+# @click.argument('model_name', type=str)
+# @click.argument('data_dir', type=click.Path(exists=True))
+# @click.option('--rebuild-vocab/--no-rebuild-vocab', default=False)
+# @click.option('--vocab-size', default=40000)
+# @click.option('--max-seq-len', default=64)
+# @click.option('--file-limit', type=int, default=None,
+        # help="Number of examples per file (community).")
+
+model_dir = 'model/synth'
+model_name = 'bigram'
+data_dir = 'data/synth_data'
+rebuild_vocab = False
+vocab_size = 40000
+max_seq_len = 64
+file_limit = None
+
+model_dir = Path(model_dir)
+data_dir = Path(data_dir)
+save_dir = model_dir/model_name
+
+util.mkdir(model_dir)
+util.mkdir(save_dir)
+
+log = util.create_logger('ngram', save_dir/'ngram.log', True)
+
+dataset, fields = data.load_data_and_fields(data_dir, model_dir,
+        max_seq_len, file_limit, vocab_size, rebuild_vocab)
+
+vocab_size = len(fields['text'].vocab.itos)
+n_comms = len(fields['community'].vocab.itos)
+comm_unk_idx = fields['community'].vocab.stoi['<unk>']
+text_pad_idx = fields['text'].vocab.stoi['<pad>']
+log.info(f"Loaded {len(dataset)} examples.")
+
+random.seed(42)
+random_state = random.getstate()
+train_data, val_data, test_data = dataset.split(split_ratio=[0.8,0.1,0.1], stratified=True, strata_field='community', random_state=random_state)
+log.info(f"Splits: train: {len(train_data)} val: {len(val_data)} test: {len(test_data)} ")
+
+# bigram counts
+C = np.zeros((n_comms,vocab_size,vocab_size),int)
+
+for example in train_data:
+    text = fields['text'].process([example.text]).numpy().squeeze()
+    comm = fields['community'].vocab.stoi[example.community]
+    for bigram in iter_ngrams(text, 2):
+        C[comm][bigram[0]][bigram[1]] += 1 # plus one smoothing... (TODO: laplace? good-turing? something else?) 
+
+
+
