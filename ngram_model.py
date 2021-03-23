687635ac4cec10578a49ea6fcdd5c23c8f09cf9f
Bill Noble <winobes@gmail.com>
2021-03-23 17:45:40 -0400

    scale ngram model (eliminate big arrays)

--- ngram_model.py
+++ ngram_model.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import util
 import data
-import random
 import itertools
 import numpy as np
 import pandas as pd
 import click
-import os
+from collections import Counter, defaultdict
+import pickle
 
 def iter_ngrams(text, n=2):
     ts = itertools.tee(text, n)
@@ -21,51 +21,60 @@ def iter_examples(n, data):
     bos_int = fields['text'].vocab[bos_token]
     for example in data:
         text = fields['text'].process([example.text]).numpy().squeeze()
-        comm = fields['community'].vocab.stoi[example.community]
+        cond = fields['community'].vocab.stoi[example.community]
         if n > 2:
             ngram_padding = np.array((bos_int,) * (n - 2))
             text = np.concatenate((ngram_padding, text)) 
-        yield comm, text
-    
-def count_ngrams(n, data):
-    n_comms = len(data.fields['community'].vocab.itos)
-    vocab_size = len(data.fields['text'].vocab.itos)
-    C = np.zeros((n_comms, *((vocab_size,) * n)),int)
-    for comm, text in iter_examples(n,data):
-        for ngram in iter_ngrams(text, n):
-            C[comm][ngram] += 1 
-    return C
+        yield cond, text
 
-def build_lm(C, alpha):
-    n_comms = C.shape[0]
-    vocab_size = C.shape[1]
-    n = len(C.shape) - 1
-    divisor_shape = (n_comms, *((vocab_size,) * (n - 1)), 1)
-    P = C / C.sum(axis=n).reshape(*divisor_shape)
-    P = np.nan_to_num(P, 0) # zeros where the ngram prefix has 0 prob
-    P_smooth_unnorm = P + alpha 
-    P_smooth = P_smooth_unnorm / P_smooth_unnorm.sum(axis=n).reshape(*divisor_shape)
-    return P_smooth
+class CondNGramLM():
 
-def example_nll(n, P, comm, text):
+    def __init__(self, N, n_conds, vocab_size, alpha, C_w=None, C=None):
+        self.N = N
+        self.n_conds = n_conds
+        self.vocab_size = vocab_size
+        self.C_w = C_w if C_w else {cond: defaultdict(Counter) for cond in range(n_conds)} 
+        self.C = C if C else {cond: Counter() for cond in range(n_conds)}
+        self.alpha = alpha
+
+    def prob(self, cond, ngram_prefix, w, alpha=None):
+        alpha = alpha if alpha else self.alpha
+        return ((self.C_w[cond][ngram_prefix][w] + alpha) / 
+               (self.C[cond][ngram_prefix] + self.vocab_size * alpha))
+
+    def fit(self, data):
+        for cond, text in iter_examples(self.N, data):
+            for ngram in iter_ngrams(text, self.N):
+                prefix, w = ngram[:-1], ngram[-1]
+                self.C_w[cond][prefix][w] += 1
+                self.C[cond][prefix] += 1
+        return self
+
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def load(cls, filename):
+        with open('filename', 'rb') as f:
+            return pickle.load(f)
+
+def example_nll(lm, cond, text, alpha=None):
+    n = lm.N
     p = []
-    if n == 1:
-        for y_hat in text[n-1:]:
-            p.append(P[comm][y_hat])
-    else:
-        for y_hat, ngram in zip(text[n-1:], iter_ngrams(text, n-1)):
-            p.append(P[comm][ngram][y_hat])
+    for y_hat, ngram_prefix in zip(text[lm.N-1:], iter_ngrams(text, lm.N-1)):
+        p.append(lm.prob(cond, ngram_prefix, y_hat, alpha=alpha))
     p = np.array(p)
     return -np.log(p)
 
-def test_lm(P, data):
+def test_lm(lm, data, alpha=None):
     results = []
-    n = len(P.shape) - 1
-    for comm, text in iter_examples(n,data):
-        nll = example_nll(n, P, comm,text)
+    N = lm.N 
+    for cond, text in iter_examples(N, data):
+        nll = example_nll(lm, cond, text, alpha)
         mean_nll = nll.sum() / len(nll)
         ppl = np.exp(mean_nll)
-        results.append({'comm': comm, 'ppl': ppl})
+        results.append({'cond': cond, 'ppl': ppl})
     df = pd.DataFrame(results)
     return df
 
@@ -84,10 +93,9 @@ def grid_search(func, n_points, min_value, max_value, n_iters, log):
         min_value, max_value, best_value = grid_search_iter(min_value, max_value)
     return best_value
 
-def test_alpha(C, alpha, test_data):
-    P = build_lm(C, alpha)
-    results = test_lm(P, test_data) 
-    return float(results.groupby('comm').mean('ppl').mean()) # macro average ppl
+def test_alpha(lm, alpha, test_data):
+    results = test_lm(lm, test_data, alpha=alpha) 
+    return float(results.groupby('cond').mean('ppl').mean()) # macro average ppl
 
 @click.command()
 @click.argument('model_family_dir', type=click.Path(exists=False))
@@ -116,7 +124,7 @@ def cli(ctx, model_family_dir, data_dir, ngram_size, max_seq_len, vocab_size, fi
     fields = data.load_fields(model_family_dir, data_dir, vocab_size)
     train_data = data.load_data(data_dir, fields, 'train', max_seq_len, file_limit)
     vocab_size = len(fields['text'].vocab.itos)
-    comm_vocab_size = len(fields['community'].vocab.itos)
+    cond_vocab_size = len(fields['community'].vocab.itos)
     text_pad_idx = fields['text'].vocab.stoi['<pad>']
     dev_data = data.load_data(data_dir, fields, 'dev', max_seq_len, None)
     log.info(f"Loaded {len(train_data)} train and {len(dev_data)} dev examples.")
@@ -124,21 +132,20 @@ def cli(ctx, model_family_dir, data_dir, ngram_size, max_seq_len, vocab_size, fi
     text_pad_idx = fields['text'].vocab.stoi['<pad>']
     vocab_size = len(fields['text'].vocab.stoi)
 
-    log.info("Counting n-grams in the training data")
-    C = count_ngrams(ngram_size, train_data)
+    lm = CondNGramLM(ngram_size, cond_vocab_size, vocab_size, None)
+    log.info("Fitting n-gram model to the training data")
+    lm.fit(train_data)
     log.info("Finding optimal smoothing parameter")
 
     min_alpha = 1 / (vocab_size**2)
     max_alpha = 1
     grid_points = 10
-    alpha = grid_search(lambda x: test_alpha(C, x, val_data), grid_points, min_alpha, max_alpha, gridsearch_iters, log)
+    alpha = grid_search(lambda x: test_alpha(lm, x, dev_data), grid_points, min_alpha, max_alpha, gridsearch_iters, log)
+    lm.alpha = alpha
+    lm.save(model_dir/'model')
 
-    P = build_lm(C, alpha)
-    np.save(model_dir/'model', P)
-    with open(os.path.join(model_dir, 'smoothing-alpha.txt'), 'w') as f:
-        f.write(f'{alpha:0.8f}')
-
-    df = test_lm(P, test_data)
+    test_data = data.load_data(data_dir, fields, 'test', max_seq_len, None)
+    df = test_lm(lm, test_data)
     df.to_pickle(results_file)
 
 if __name__ == '__main__':
