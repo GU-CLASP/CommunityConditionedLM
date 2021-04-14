f9f573c633c2f771d84d4dcdd7135c9baec3aa6c
Bill Noble <winobes@gmail.com>
2021-04-14 15:17:26 +0200

    improve trainig restarting

--- util.py
+++ util.py
@@ -3,13 +3,16 @@ import logging
 import click
 from pathlib import Path
 import json
+import re
 
 def get_community_label(file_path):
     base = os.path.basename(file_path)
     return base.split('.')[0]
 
 def get_communities(data_dir):
-    return [get_community_label(f) for f in  os.listdir(data_dir) if f.endswith('.train.txt')]
+    comms = [get_community_label(f) for f in  os.listdir(data_dir) if f.endswith('.train.txt')]
+    comms.sort()
+    return comms
 
 def data_filename(data_dir, split, comm):
     return Path(data_dir)/f'{comm}.{split}.txt'
@@ -46,3 +49,16 @@ def iter_data(data_dir, split, file_limit=None):
 def iter_file(filename):
     for i,line in enumerate(open(filename).readlines()):
         yield line.rstrip('\n')
+
+
+def read_logged_val_ppls(model_dir):
+    log_file = os.path.join(model_dir, 'training.log') 
+    log_regex = re.compile("Epoch\s+(\d+)\s+\|\s+val loss\s+(\d+\.\d+)\s+\|\s+ppl\s+(\d+\.\d+)")
+    log_contents = open(log_file).read()
+    ppls = {}
+    for epoch, loss, ppl in re.findall(log_regex, log_contents):
+        ppls[int(epoch)] = float(ppl)
+    ppls = [ppls[i+1] for i in range(len(ppls))]
+    return ppls
+
+
